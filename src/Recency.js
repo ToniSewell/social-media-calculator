45d90b1e96045d6408b9ea0bc09d20cb0f88c567
ToniSewell <antoniasewell@gmail.com>
2026-05-05 12:26:53 +0100

    removed Mo Recency

--- src/Recency.js
+++ src/Recency.js
@@ -46,12 +46,6 @@ export default function RecencyPage() {
 
   return (
     <PageLayout className="likes-page">
-      <UserSection
-        title="User"
-        name="Mo"
-        imgSrc={process.env.PUBLIC_URL + '/profile_pic.png'}
-        bio="Mo enjoys doing drawing and painting in his free time. He plays the flute in the school band and is a massive fan of Selena Swift!"
-      />
 
       {/* Middle Column */}
       <div className="column card builder-section">
